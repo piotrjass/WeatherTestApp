315e40fda9e8ad55b264fca86b875e82dc98ed82
piotrjass <131187708+piotrjass@users.noreply.github.com>
2025-06-04 15:13:35 +0200

    WEA-1 dodanie

--- WeatherTestApp/Program.cs
+++ WeatherTestApp/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-// some changes
+// some changes adssddsa
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
